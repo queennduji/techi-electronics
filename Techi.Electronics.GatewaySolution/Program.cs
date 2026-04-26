8b516ab1a694cfdd6fdd00c31611b4e5e0fdf4f9
Queen Nduji <ndujiqueen@email.com>
2026-04-26 16:11:51 -0700

    Add response compression to API Gateway

--- Techi.Electronics.GatewaySolution/Program.cs
+++ Techi.Electronics.GatewaySolution/Program.cs
@@ -6,7 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddAppAuthentication();
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+});
 var app = builder.Build();
+app.UseResponseCompression();
 app.MapGet("/", () => "Hello World!");
 app.UseOcelot().GetAwaiter().GetResult();
 app.Run();
